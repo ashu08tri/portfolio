a77429c67cbed83c1e4cd290b91e5ca05a8827aa
Ashutosh Tripathi <63796943+ashu08tri@users.noreply.github.com>
2024-03-21 20:27:41 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AnimateWrapper children={children} />
+        <AnimateWrapper>
+              {children}
+          </AnimateWrapper>
         </body>
     </html>
   );
